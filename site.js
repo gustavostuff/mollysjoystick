1552ff824d685883cb9978d09a3048f9c7b7a5cf
tavuntu <ing.glaragomez@gmail.com>
2021-09-27 18:12:46 -0700

    inital commit

---
+++ site.js
@@ -0,0 +1,17 @@
+(function () {
+	let wordIndex = 1;
+	function geWordItem(index) {
+		return document.querySelector(`.words-list .title-word:nth-child(${ index }`);
+	}
+
+	setInterval(function () {
+		let currentWord = geWordItem(wordIndex);
+		
+		currentWord.classList.add('hidden');
+		wordIndex++;
+		wordIndex = (wordIndex > 5) ? 1 : wordIndex;
+
+		let nextWord = geWordItem(wordIndex);
+		nextWord.classList.remove('hidden');
+	}, 2000);
+})();
